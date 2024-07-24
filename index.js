1a9e013d1c0d7f86bfaca81258d2432ca2724420
Narasimha6223 <vaddalanarasimha@gmail.com>
2024-07-24 16:21:29 +0530

    no changhe

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const product = require('./modals/product')
 app.use(cors())
 app.use(express.json())
 
+
+app.get('/' , (req,res)=>{
+    return res.send("<h1>Hello</h1>")
+})
+
 app.use('/products' , require('./routes/productsRoute'))
 app.use('/auth' , require('./routes/Auth'))
 
