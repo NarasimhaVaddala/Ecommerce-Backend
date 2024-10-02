8a7ce6d85331322b574f93082ec451a24e1c3b44
Narasimha Vaddala <119875112+NarasimhaVaddala@users.noreply.github.com>
2024-10-02 22:58:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 
-// let url = "mongodb+srv://narasimha:narasimha@cluster1.ktyz1yg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+ let url = "mongodb+srv://narasimha:narasimha@cluster1.ktyz1yg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
 
-let url = "mongodb://localhost:27017/ecommerce"
+//let url = "mongodb://localhost:27017/ecommerce"
 
 mongoose.connect(url).then(()=>console.log("Connected to Db")).catch((err)=>{console.log(err.message)})
 
